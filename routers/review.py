d9b24375d934fec4465388a177dde2dd8323a3db
Alya Azhar Agharid <alya.azhar@ui.ac.id>
2023-05-01 21:43:43 +0700

    Change the respon on review when error happened

--- routers/review.py
+++ routers/review.py
@@ -24,54 +24,62 @@ def get_reviews_by_user(user: User = Depends(verify_jwt)):
 def get_reviews_by_bookid(book_id: int):
     reviews = supabase.table('book_review').select('*', count='exact').eq('book_id', book_id).execute()
     if len(reviews.data) == 0:
-        return JSONResponse({'message':'Book ID Not Found'})
+        return HTTPException(status_code=404, detail="Book ID Not Found")
     return reviews.data
 
 @router.post("/add/{book_id}", response_model=Review)
 async def create_review(book_id:int, review: ReviewCreate, user: User = Depends(verify_jwt)): 
+    if review.rating < 0 or review.rating > 5 or not review.review_text.strip():
+        raise HTTPException(status_code=400, detail="Invalid Input. Unable to Add Review.")
+    
     reviews = supabase.table('book_review').select('*', count='exact').eq('book_id', book_id).execute()
     if len(reviews.data) == 0:
-        return JSONResponse({'message':'Book ID Not Found'})
+        raise HTTPException(status_code=404, detail="Book ID Not Found")
     
-    if 0 <= review.rating <= 5 and review.review_text.strip() != '':
-        data, count = supabase.table('book_review').insert({"book_id": book_id, "user_id":user.username, "rating":review.rating, "review_text":review.review_text}).execute()
-        data_dict = {
-            'id': data[1][0]['id'],
-            'created_at': data[1][0]['created_at'][0:10],
-            'book_id': data[1][0]['book_id'],
-            'user_id': data[1][0]['user_id'],
-            'rating': data[1][0]['rating'],
-            'review_text': data[1][0]['review_text']
-        }
-        return data_dict
-    return JSONResponse({'message': "Add Review Failed. Data Invalid."})
+    data, count = supabase.table('book_review').insert({
+        "book_id": book_id, 
+        "user_id": user.username, 
+        "rating": review.rating, 
+        "review_text": review.review_text
+    }).execute()
+    
+    data_dict = {
+        'id': data[1][0]['id'],
+        'created_at': data[1][0]['created_at'][0:10],
+        'book_id': data[1][0]['book_id'],
+        'user_id': data[1][0]['user_id'],
+        'rating': data[1][0]['rating'],
+        'review_text': data[1][0]['review_text']
+    }
+    return data_dict
 
 @router.put("/update/{review_id}", response_model=Review)
 async def update_review(review_id: int, review: ReviewCreate, user: User = Depends(verify_jwt)):
+    if review.rating < 0 or review.rating > 5 or not review.review_text.strip():
+        raise HTTPException(status_code=400, detail="Invalid Input. Unable to Change Review.")
+
     data_review = supabase.table('book_review').select("*", count='exact').eq('user_id', user.username).eq('id', review_id).execute()
     if len(data_review.data) == 0 :
-        return JSONResponse({'message':'Review ID Not Found'})
-    if 0 <= review.rating <= 5 and review.review_text.strip() != '':
-        updated_review = supabase.table('book_review').update({"rating": review.rating, "review_text": review.review_text}).eq('id', review_id).execute()
-        if updated_review:
-            return JSONResponse({'message': f"Review with id {review_id} Successfully Changed."})
-    return JSONResponse({'message': "Unable to Change Review. Try Again."})
+        raise HTTPException(status_code=404, detail="Review ID Not Found")
+    
+    updated_review = supabase.table('book_review').update({"rating": review.rating, "review_text": review.review_text}).eq('id', review_id).execute()
+    return JSONResponse({'detail': f"Review with id {review_id} Successfully Changed."})
 
 @router.delete("/delete/{review_id}")
 def delete_review(review_id: int, user: User = Depends(verify_jwt)):
     data_review = supabase.table('book_review').select("*", count='exact').eq('user_id', user.username).eq('id', review_id).execute()
     if len(data_review.data) == 0 :
-        return JSONResponse({'message':'Review ID Not Found'})
+        return HTTPException(status_code=404, detail="Review ID Not Found")
     deleted_review = supabase.table('book_review').delete().eq('id', review_id).execute()
     if deleted_review:
-        return JSONResponse({'message': f"Review with id {review_id} Successfully Deleted."})
-    return JSONResponse({'message': "Unable to Delete Review. Try Again."})
+        return JSONResponse({'detail': f"Review with id {review_id} Successfully Deleted."})
+    return HTTPException(status_code=400, detail="Unable to Delete Review.")
 
 @router.get("/avg-rate/{book_id}")
 def get_avg_rate(book_id: int):
     reviews = supabase.table('book_review').select('rating').eq('book_id', book_id).execute()
     if len(reviews.data) == 0:
-        return JSONResponse({'message':'Book ID Not Found'})
+        return HTTPException(status_code=404, detail="Book ID Not Found")
     total_reviews = len(reviews.data)
     total_rating = sum([review['rating'] for review in reviews.data])
     avg_rating = round(total_rating/total_reviews, 1) if total_reviews != 0 else 0
